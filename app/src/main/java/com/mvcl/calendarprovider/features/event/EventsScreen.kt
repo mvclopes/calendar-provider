46a6698a462085d86f6df6420eb0b0d88a18ef1e
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-17 18:57:15 -0300

    Feature: punctual change

--- app/src/main/java/com/mvcl/calendarprovider/features/event/EventsScreen.kt
+++ app/src/main/java/com/mvcl/calendarprovider/features/event/EventsScreen.kt
@@ -2,6 +2,7 @@ package com.mvcl.calendarprovider.features.event
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,7 +15,9 @@ import androidx.compose.ui.Modifier
 fun EventsScreen(calendarId: Long) {
     Scaffold {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
             contentAlignment = Alignment.Center
         ) {
             Text(text = "Calendar ID: $calendarId")
