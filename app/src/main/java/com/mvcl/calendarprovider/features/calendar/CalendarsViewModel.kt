5b68ae0eeac5ff1ca1dd9b9427bce4994ea77e3d
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-17 17:49:56 -0300

    Feature: created calendar screen and its viewModel

---
+++ app/src/main/java/com/mvcl/calendarprovider/features/calendar/CalendarsViewModel.kt
@@ -0,0 +1,34 @@
+package com.mvcl.calendarprovider.features.calendar
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mvcl.calendarprovider.calendar.CalendarProvider
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class CalendarsViewModel(
+    private val calendarProvider: CalendarProvider
+): ViewModel() {
+
+    var state = MutableStateFlow<CalendarViewState>(CalendarViewState.Idle)
+        private set
+
+    init {
+        getCalendars()
+    }
+
+    private fun getCalendars() {
+        viewModelScope.launch {
+        state.emit(CalendarViewState.Loading)
+            delay(1500)
+            runCatching {
+                calendarProvider.getCalendars()
+            }.onSuccess {
+                state.emit(CalendarViewState.Success(it))
+            }.onFailure {
+                state.emit(CalendarViewState.Error)
+            }
+        }
+    }
+}
