734a7872939016ea33acd7122eebefbe837651bb
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-17 16:35:38 -0300

    Feature: added permission and calendar provider dependency in app build gradle

--- app/src/main/java/com/mvcl/calendarprovider/MainActivity.kt
+++ app/src/main/java/com/mvcl/calendarprovider/MainActivity.kt
@@ -1,46 +1,147 @@
 package com.mvcl.calendarprovider
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.mvcl.calendarprovider.calendar.CalendarProviderImpl
+import com.mvcl.calendarprovider.calendar.model.CalendarEntity
+import com.mvcl.calendarprovider.calendar.model.CalendarProvider
 import com.mvcl.calendarprovider.ui.theme.CalendarProviderTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val calendarProvider: CalendarProvider by lazy {
+        CalendarProviderImpl(
+            contentResolver = contentResolver
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalendarProviderTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CalendarPermissions { calendarProvider.getCalendars() }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CalendarPermissions(onCalendars: () -> List<CalendarEntity>) {
+    val calendarPermissions = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.READ_CALENDAR,
+            Manifest.permission.WRITE_CALENDAR,
+        )
     )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text(
+                        text = "Available calendars",
+                        color = Color.White
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Default.Add, contentDescription = null)
+            }
+
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            if (calendarPermissions.allPermissionsGranted) {
+                val calendars = onCalendars()
+                CalendarList(calendars)
+            } else {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    val textToShow = if (calendarPermissions.shouldShowRationale) {
+                        "The camera is important for this app. Please grant the permission."
+                    } else {
+                        "Camera permission required for this feature to be available. " +
+                                "Please grant the permission"
+                    }
+                    Text(textToShow)
+                    Button(onClick = { calendarPermissions.launchMultiplePermissionRequest() }) {
+                        Text("Request permission")
+                    }
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    CalendarProviderTheme {
-        Greeting("Android")
+fun CalendarList(calendars: List<CalendarEntity>) {
+    LazyColumn {
+        items(calendars) { calendar ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(calendar.color)
+                ),
+                onClick = {}
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(text = "Display name: ${calendar.displayName}")
+                    Text(text = "Owner account: ${calendar.ownerAccount}")
+                }
+            }
+        }
     }
 }
