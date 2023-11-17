78fb815efad9ed61aa1255f919c9a68c052e7e16
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-17 17:51:11 -0300

    Feature: created calendar view state

---
+++ app/src/main/java/com/mvcl/calendarprovider/features/calendar/CalendarViewState.kt
@@ -0,0 +1,10 @@
+package com.mvcl.calendarprovider.features.calendar
+
+import com.mvcl.calendarprovider.calendar.model.CalendarEntity
+
+sealed class CalendarViewState {
+    object Error: CalendarViewState()
+    object Idle: CalendarViewState()
+    object Loading: CalendarViewState()
+    data class Success(val calendars: List<CalendarEntity>): CalendarViewState()
+}
