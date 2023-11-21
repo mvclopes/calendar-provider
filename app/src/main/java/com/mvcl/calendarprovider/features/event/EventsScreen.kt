0bc5b75d8da9c980a12df126900eb23d98829502
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-21 11:56:59 -0300

    Feature: changes in event's screen and navigation

--- app/src/main/java/com/mvcl/calendarprovider/features/event/EventsScreen.kt
+++ app/src/main/java/com/mvcl/calendarprovider/features/event/EventsScreen.kt
@@ -1,26 +1,123 @@
 package com.mvcl.calendarprovider.features.event
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mvcl.calendarprovider.components.LoadingInCenter
+import com.mvcl.calendarprovider.event.model.EventEntity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EventsScreen(calendarId: Long) {
-    Scaffold {
+fun EventsScreen(
+    state: EventViewState,
+    onBackPressed: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text(
+                        text = "Available events",
+                        color = Color.White
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { onBackPressed() }
+                    ) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            tint = Color.White,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            when (state) {
+                EventViewState.Error,
+                EventViewState.Idle -> Unit
+
+                EventViewState.Loading -> LoadingInCenter()
+
+                is EventViewState.Success -> EventList(
+                    events = state.events,
+                    onEventClicked = {}
+                )
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EventList(
+    events: List<EventEntity>,
+    onEventClicked: (Long) -> Unit
+) {
+    if (events.isEmpty()) {
         Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
+            modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "Calendar ID: $calendarId")
+            Text(
+                text = "No available events",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    } else {
+        LazyColumn {
+            items(events) { event ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(event.displayColor)
+                    ),
+                    onClick = { onEventClicked(event.id) }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = event.title)
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/mvcl/calendarprovider/features/event/navigation/EventsNavigation.kt
+++ app/src/main/java/com/mvcl/calendarprovider/features/event/navigation/EventsNavigation.kt
@@ -1,16 +1,21 @@
 package com.mvcl.calendarprovider.features.event.navigation
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.mvcl.calendarprovider.features.event.EventsScreen
+import com.mvcl.calendarprovider.features.event.EventsViewModel
 import com.mvcl.calendarprovider.navigation.AppRoutes
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
 
 private const val CALENDAR_ID_ARG = "calendarId"
 
-fun NavGraphBuilder.eventsScreen() {
+fun NavGraphBuilder.eventsScreen(navController: NavController) {
     composable(
         route = "${AppRoutes.EVENT_SCREEN.name}/{$CALENDAR_ID_ARG}",
         arguments = listOf(
@@ -18,7 +23,13 @@ fun NavGraphBuilder.eventsScreen() {
         )
     ) { backStackEntry ->
         val calendarId = backStackEntry.arguments?.getLong(CALENDAR_ID_ARG) ?: 0
-        EventsScreen(calendarId)
+        val viewModel = koinViewModel<EventsViewModel>{ parametersOf(calendarId) }
+        val state by viewModel.state.collectAsState()
+
+        EventsScreen(
+            state = state,
+            onBackPressed = { navController.popBackStack() }
+        )
     }
 }
 
