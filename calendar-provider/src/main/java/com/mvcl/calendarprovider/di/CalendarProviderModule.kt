9480cce72aa69d60f6ed30928ec4636b9e675d1c
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-21 15:47:05 -0300

    Feature: added reminder provider in DI layer

--- calendar-provider/src/main/java/com/mvcl/calendarprovider/di/CalendarProviderModule.kt
+++ calendar-provider/src/main/java/com/mvcl/calendarprovider/di/CalendarProviderModule.kt
@@ -4,21 +4,26 @@ import com.mvcl.calendarprovider.calendar.CalendarProvider
 import com.mvcl.calendarprovider.calendar.CalendarProviderImpl
 import com.mvcl.calendarprovider.event.EventProvider
 import com.mvcl.calendarprovider.event.EventProviderImpl
+import com.mvcl.calendarprovider.reminder.ReminderProvider
+import com.mvcl.calendarprovider.reminder.ReminderProviderImpl
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
 object CalendarProviderModule {
+    private fun Scope.getContentResolver() = androidContext().contentResolver
+
     val module = module {
         factory<CalendarProvider> {
-            CalendarProviderImpl(
-                contentResolver = androidContext().contentResolver
-            )
+            CalendarProviderImpl(contentResolver = getContentResolver())
         }
 
         factory<EventProvider> {
-            EventProviderImpl(
-                contentResolver = androidContext().contentResolver
-            )
+            EventProviderImpl(contentResolver = getContentResolver())
+        }
+
+        factory<ReminderProvider> {
+            ReminderProviderImpl(contentResolver = getContentResolver())
         }
     }
 }
