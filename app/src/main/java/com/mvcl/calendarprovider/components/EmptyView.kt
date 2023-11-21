10a36518d44556a2b5d8dbe1181180e155d8659f
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-21 12:17:50 -0300

    Feature: created emptyView and errorView components

---
+++ app/src/main/java/com/mvcl/calendarprovider/components/EmptyView.kt
@@ -0,0 +1,24 @@
+package com.mvcl.calendarprovider.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EmptyView(message: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = message,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
